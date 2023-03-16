e182ee5109fac611e4fad39f7b5d860f71d55601
Artem Zaiarnyi <artem_zaiarnyi@epam.com>
2023-03-16 08:28:31 +0200

    Added main screen and navigation

--- App.js
+++ App.js
@@ -1,19 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <NavigationContainer></NavigationContainer>;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
